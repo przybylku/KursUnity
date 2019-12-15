1d3c09ed7b567804497a3cdc8f75654991392fd6
Jakub Przybylski <kubaa.przbylski04@gmail.com>
2019-12-15 13:38:11 +0100

    Player_movement

---
+++ KursUnity/Assets/scripts/player_movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+
+    CharacterController characterController;
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* Poruszanie się*/
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+        }
+        /* Ustawienie gravitacji*/
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        characterController.Move(moveDirection);
+    }
+}
